1b085633a45dda1a376c2ad56fd16418e17490ca
123abcdf11345 <2542594900@qq.com>
2021-12-05 14:57:09 +0800

    v1.3.0 fix window will be hiden if another app is opening ;add tips

--- stopclass.py
+++ stopclass.py
@@ -5,24 +5,26 @@ import sys
 import json
 import requests
 import os
+import threading
 class TestTime(object):
     def __init__(self, master=None):
         self.root = master
         self.updatetime()
 
     def updatetime(self):
-        self.labelE = Label(self.root, text='\n  距离晚自习开始还有：',font = ('Arial' , 75))
-        self.labelE.bind_all('<Control-Alt-c>', self.exit)#注册快捷键
+        self.labelE = Label(self.root, text='\n  距离srt开始还有：',font = ('Arial' , 75))
+        self.labelE.bind_all('<F9>', self.exit)#注册快捷键
 
         self.labelE.pack()
         self.labelF = Label(self.root, text="",font = ('Arial' , 75),fg='red')#空label等待更新
         self.labelF.pack()
         self.labelT = Label(self.root, text="",font = ('Arial' , 25))#空label等待更新
         self.labelT.pack()
-        self.labelG = Label(self.root,text=history,font = ('Arial' , 13))
+        self.labelG = Label(self.root,text=history,font = ('Arial' , 18))
         self.labelG.pack()
-        self.labelH = Label(self.root,text=full_message,font = ('Arial' , 13))
+        self.labelH = Label(self.root,text=full_message,font = ('Arial' , 18))
         self.labelH.pack()
+        self.top()
         self.updateC()
 
     def updateC(self):
@@ -36,23 +38,25 @@ class TestTime(object):
         # print(t)
         if t == ['0', '0', ':', '0', '0']:
             try:
-                if os.system('shutdown -s -t 1') != 0:
-                    showwarning('Access Error','Could Not Shutdown')
+                os.system('shutdown -s -t 1')
             finally:
                 sys.exit()
         self.root.after(1000, self.updateC)
     def exit(self,message):
-        if askyesno("GUI-MESSAGEBOX","快捷键捕捉，是否退出?") == True:
+        if askyesno("拖堂防沉迷系统","快捷键捕捉，是否退出?") == True:
             sys.exit()
         else:
             pass
-
+    def top(self):
+        self.root.lift()
+        self.root.after(50,self.top)#每隔50ms把root窗口置顶
 def showit():
     str_message='''
-新华社北京7月24日电 近日，中共中央办公厅、国务院办公厅印发了《关于进一步减轻义务教育阶段学生作业负担和校外培训负担的意见》，并发出通知，要求各地区各部门结合实际认真贯彻落实。
+新华社北京7月24日电 \n近日，中共中央办公厅、国务院办公厅印发了《关于进一步减轻义务教育阶段学生作业负担和校外培训负担的意见》，并发出通知，要求各地区各部门结合实际认真贯彻落实。
 '''
-    showwarning('Warning',str_message)
-
+    showwarning('拖堂防沉迷系统',str_message)
+def timetoclose(window):
+    window.destroy()
 if __name__ == '__main__':
     full_message=''
     admin_message=requests.get('https://class.api.askdream.top:88/note.html')
@@ -78,10 +82,16 @@ if __name__ == '__main__':
     root.title('拯救五班学生于水火之中')
     #全屏锁定
     root.attributes('-fullscreen', True)
+    message2=Label(root,text='根据本班最新强化的防沉迷规则，当前时段您不能继续讲课',font = ('Arial' , 17))
+    message2.pack()
+    threading.Timer(5, timetoclose, args=(message2,)).start()#定时器线程，5s后关闭防沉迷提示
     #倒计时类实例
     TestTime(root)
-    #
+    root.lift()
+    #拦截窗口关闭事件
     root.protocol("WM_DELETE_WINDOW",showit)
-    #exit_button=Button(root,text='EXIT',command=sys.exit)
-    #exit_button.pack()
+    #调试button
+    exit_button=Button(root,text='EXIT',command=sys.exit)
+    exit_button.pack()
+    #调试结束
     root.mainloop()
