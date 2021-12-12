2796492ea102262456e11209f332c76583a10702
123abcdf11345 <2542594900@qq.com>
2021-12-12 20:07:07 +0800

    v1.4.0:Change API,Add Debug Mode,Chance hot key,Change HTTP request header,fix some bug

--- stopclass.py
+++ stopclass.py
@@ -6,15 +6,33 @@ import json
 import requests
 import os
 import threading
+headers={'Accept':'application/json','User-Agent':'StopClass Client 2.0'}
 class TestTime(object):
     def __init__(self, master=None):
         self.root = master
         self.updatetime()
-
+    def tryclose(self):
+        self.w_com=Toplevel()
+        self.w_com.title('Result')
+        self.text_com=Text(self.w_com)
+        self.text_com.pack(side=TOP, fill=BOTH)
+        self.text_com.insert(END,os.popen('D:/closedisplay.bat').readline())
+    def debug(self,event):
+        self.w_debug=Toplevel()
+        self.w_debug.title('Debug Mode')
+        self.buttonA=Button(self.w_debug,text='Close Display(BATE)',command=self.tryclose)
+        self.buttonA.pack()
+        self.buttonB=Button(self.w_debug,text='Close App',command=sys.exit)
+        self.buttonB.pack()
+        self.buttonC=Button(self.w_debug,text='Shutdown System',command=lambda:os.system('shutdown -s'))
+        self.buttonC.pack()
+        self.buttonD=Button(self.w_debug,text='Reboot System',command=lambda:os.system('shutdown -r'))
+        self.buttonD.pack()
+        self.LabelAb=Label(self.w_debug,text='Copyright © 2021 GPL-3.0 WJZ\nPowered by Tkinter on Python\nVersion:V1.4.0')
+        self.LabelAb.pack()
     def updatetime(self):
         self.labelE = Label(self.root, text='\n  距离晚自习开始还有：',font = ('Arial' , 75))
-        self.labelE.bind_all('<F9>', self.exit)#注册快捷键
-
+        self.labelE.bind_all('<F10>', self.debug)
         self.labelE.pack()
         self.labelF = Label(self.root, text="",font = ('Arial' , 75),fg='red')#空label等待更新
         self.labelF.pack()
@@ -24,7 +42,6 @@ class TestTime(object):
         self.labelG.pack()
         self.labelH = Label(self.root,text=full_message,font = ('Arial' , 18))
         self.labelH.pack()
-        self.top()
         self.updateC()
 
     def updateC(self):
@@ -38,18 +55,11 @@ class TestTime(object):
         # print(t)
         if t == ['0', '0', ':', '0', '0']:
             try:
-                os.system('shutdown -s -t 1')
+                a=os.popen('closedisplay')
+                os.system('shutdown -s')
             finally:
                 sys.exit()
         self.root.after(1000, self.updateC)
-    def exit(self,message):
-        if askyesno("拖堂防沉迷系统","快捷键捕捉，是否退出?") == True:
-            sys.exit()
-        else:
-            pass
-    def top(self):
-        self.root.lift()
-        self.root.after(50,self.top)#每隔50ms把root窗口置顶
 def showit():
     str_message='''
 新华社北京7月24日电 \n近日，中共中央办公厅、国务院办公厅印发了《关于进一步减轻义务教育阶段学生作业负担和校外培训负担的意见》，并发出通知，要求各地区各部门结合实际认真贯彻落实。
@@ -59,15 +69,24 @@ def timetoclose(window):
     window.destroy()
 if __name__ == '__main__':
     full_message=''
-    admin_message=requests.get('https://class.api.askdream.top:88/note.html')
-    admin_message.encoding='utf-8'
-    admin_message=admin_message.text
+    if len(str(datetime.now().month))==1:
+        month='0'+str(datetime.now().month)
+    else:
+        month=str(datetime.now().month)
+    if len(str(datetime.now().day))==1:
+        day='0'+str(datetime.now().day)
+    else:
+        day=str(datetime.now().day)
+    str_date=month+day
+    print(str_date)
+    
     try:
-        if admin_message  != 'display':
-            history=admin_message
-        else:
+        admin_message=requests.get('https://class.api.askdream.top:88/v2/list',headers=headers).json()
+        try:
+            admin_message=admin_message[str_date]
+        except KeyError:
             try:
-                data=requests.get('https://www.ipip5.com/today/api.php?type=json').json()['result']
+                data=requests.get('https://www.ipip5.com/today/api.php?type=json',headers=headers).json()['result']
             except:
                 history=''
             else:
@@ -76,22 +95,25 @@ if __name__ == '__main__':
                     today_message=use_data['year']+'年，'+use_data['title']
                     full_message=full_message+'\n'+today_message
                 history='\n\n\n\n历史上的今天'
+        else:
+            history='\n\n'+admin_message            
     except:
         history=''
     root = Tk()
-    root.title('拯救五班学生于水火之中')
+    root.title('拖堂防沉迷系统')
     #全屏锁定
     root.attributes('-fullscreen', True)
-    message2=Label(root,text='根据本班最新强化的防沉迷规则，当前时段您不能继续讲课',font = ('Arial' , 17))
+    message2=Label(root,text='\n',font = ('Arial' , 17))
     message2.pack()
-    threading.Timer(5, timetoclose, args=(message2,)).start()#定时器线程，5s后关闭防沉迷提示
+    threading.Timer(1, timetoclose, args=(message2,)).start()#定时器线程，5s后关闭防沉迷提示
     #倒计时类实例
     TestTime(root)
-    root.lift()
+    
+    root.wm_attributes('-topmost',1)
     #拦截窗口关闭事件
     root.protocol("WM_DELETE_WINDOW",showit)
     #调试button
-#    exit_button=Button(root,text='EXIT',command=sys.exit)
-#    exit_button.pack()
+    #exit_button=Button(root,text='EXIT',command=sys.exit)
+    #exit_button.pack()
     #调试结束
     root.mainloop()
