5f24e55983bf08e5ec81ab2b1c59c7351836dfaa
123abcdf11345 <2542594900@qq.com>
2021-12-26 15:41:51 +0800

    V1.4.1 - 修复了字体问题 - 修复了关闭程序时的问题 - 修复了debug模式不能正常启动的问题

--- stopclass.py
+++ stopclass.py
@@ -18,9 +18,11 @@ class TestTime(object):
         self.text_com.pack(side=TOP, fill=BOTH)
         self.text_com.insert(END,os.popen('D:/closedisplay.bat').readline())
     def debug(self,event):
+        self.root.wm_attributes('-topmost',0)#取消主窗口置顶
         self.w_debug=Toplevel()
         self.w_debug.title('Debug Mode')
-        self.buttonA=Button(self.w_debug,text='Close Display(BATE)',command=self.tryclose)
+        self.w_debug.wm_attributes('-topmost',1)#分窗口置顶
+        self.buttonA=Button(self.w_debug,text='Close Display',command=self.tryclose)
         self.buttonA.pack()
         self.buttonB=Button(self.w_debug,text='Close App',command=sys.exit)
         self.buttonB.pack()
@@ -28,17 +30,17 @@ class TestTime(object):
         self.buttonC.pack()
         self.buttonD=Button(self.w_debug,text='Reboot System',command=lambda:os.system('shutdown -r'))
         self.buttonD.pack()
-        self.LabelAb=Label(self.w_debug,text='Copyright © 2021 GPL-3.0 WJZ\nPowered by Tkinter on Python\nVersion:V1.4.0')
+        self.LabelAb=Label(self.w_debug,text='Copyright © 2021 GPL-3.0 WJZ\nPowered by Tkinter on Python\nVersion:V1.4.1')
         self.LabelAb.pack()
     def updatetime(self):
-        self.labelE = Label(self.root, text='\n  距离晚自习开始还有：',font = ('Arial' , 75))
+        self.labelE = Label(self.root, text='\n  距离晚自习开始还有：',font = ('黑体' , 75))
         self.labelE.bind_all('<F10>', self.debug)
         self.labelE.pack()
         self.labelF = Label(self.root, text="",font = ('Arial' , 75),fg='red')#空label等待更新
         self.labelF.pack()
         self.labelT = Label(self.root, text="",font = ('Arial' , 25))#空label等待更新
         self.labelT.pack()
-        self.labelG = Label(self.root,text=history,font = ('Arial' , 18))
+        self.labelG = Label(self.root,text=history,font = ('Arial' , font_number))
         self.labelG.pack()
         self.labelH = Label(self.root,text=full_message,font = ('Arial' , 18))
         self.labelH.pack()
@@ -48,26 +50,28 @@ class TestTime(object):
         # 获取当日日期，不包含时间，str
         self.nowday = datetime.now().strftime("%Y-%m-%d")
         # 字符串拼接，组成当日expect time
-        a = self.nowday + ' 18:20:00'
+        a = self.nowday + ' 18:35:00'
         self.newtime = datetime.strptime(a, "%Y-%m-%d %H:%M:%S")
         t =list(str(self.newtime - datetime.now()))[2:7]#.strftime("%Y-%m-%d %H:%M:%S")#暴力格式化
         self.labelF.configure(text=t)
         # print(t)
         if t == ['0', '0', ':', '0', '0']:
+            timetoclose(self.root)#调用函数关闭窗口
             try:
-                a=os.popen('closedisplay')
-                os.system('shutdown -s')
+                os.system('closedisplay')
+                os.system('shutdown -s -t 1')
             finally:
                 sys.exit()
-        self.root.after(1000, self.updateC)
+        self.root.after(1000, self.updateC)#1000ms更新倒计时
 def showit():
     str_message='''
 新华社北京7月24日电 \n近日，中共中央办公厅、国务院办公厅印发了《关于进一步减轻义务教育阶段学生作业负担和校外培训负担的意见》，并发出通知，要求各地区各部门结合实际认真贯彻落实。
 '''
-    showwarning('拖堂防沉迷系统',str_message)
+    showwarning('Fuck School',str_message)
 def timetoclose(window):
-    window.destroy()
+    window.destroy()#销毁窗口
 if __name__ == '__main__':
+    font_number=18
     full_message=''
     if len(str(datetime.now().month))==1:
         month='0'+str(datetime.now().month)
@@ -78,7 +82,7 @@ if __name__ == '__main__':
     else:
         day=str(datetime.now().day)
     str_date=month+day
-    print(str_date)
+    #print(str_date)
     
     try:
         admin_message=requests.get('https://class.api.askdream.top:88/v2/list',headers=headers).json()
@@ -96,19 +100,21 @@ if __name__ == '__main__':
                     full_message=full_message+'\n'+today_message
                 history='\n\n\n\n历史上的今天'
         else:
-            history='\n\n'+admin_message            
+            history='\n\n'+admin_message
+            font_number=21            
     except:
         history=''
     root = Tk()
-    root.title('拖堂防沉迷系统')
+    root.title('Fuck School')
     #全屏锁定
     root.attributes('-fullscreen', True)
+    #5秒提示
     message2=Label(root,text='\n',font = ('Arial' , 17))
     message2.pack()
-    threading.Timer(1, timetoclose, args=(message2,)).start()#定时器线程，5s后关闭防沉迷提示
+    threading.Timer(0.01, timetoclose, args=(message2,)).start()#定时器线程，5s后关闭防沉迷提示
     #倒计时类实例
     TestTime(root)
-    
+    #窗口锁定置顶
     root.wm_attributes('-topmost',1)
     #拦截窗口关闭事件
     root.protocol("WM_DELETE_WINDOW",showit)
